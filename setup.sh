7648fa3baf28f8fc15683353cc7ea37e33a9db0e
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-07-07 00:39:19 +0530

    added sample images, updated requirements,

--- setup.sh
+++ setup.sh
@@ -2,13 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"okeaditya315@gmail.com\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
